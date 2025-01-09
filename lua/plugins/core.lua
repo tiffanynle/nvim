db563145244d305290242988b0e128328aa91276
Tiffany Le <tiffanynle.cs@gmail.com>
2025-01-09 11:28:11 -0600

    Initial commit

---
+++ lua/plugins/core.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "catppuccin-mocha" },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = { preset = "classic" },
+  },
+  { "nvim-tree/nvim-web-devicons", opts = {} },
+  { "echasnovski/mini.icons", version = false },
+}
