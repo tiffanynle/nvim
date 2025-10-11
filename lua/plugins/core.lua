183132ff30017934c536dbd411506e7894af024f
Tiffany Le <78166162+tiffanynle@users.noreply.github.com>
2025-10-10 19:23:54 -0500

    Update lazyvim

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,11 +1,17 @@
 return {
   {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = { transparent_background = true, float = { transparent = true, solid = false } },
+  },
+  {
     "LazyVim/LazyVim",
     opts = { colorscheme = "catppuccin-mocha" },
   },
   {
     "folke/which-key.nvim",
-    opts = { preset = "classic" },
+    opts = { preset = "modern" },
   },
   { "nvim-tree/nvim-web-devicons", opts = {} },
   { "nvim-mini/mini.icons", version = false },
