ce735d6c153eb538673dbdef5ea5512e27b35452
Tiffany Le <78166162+tiffanynle@users.noreply.github.com>
2026-02-07 16:50:15 -0600

    update to use llama.cpp adapter

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -4,15 +4,33 @@ return {
     "nvim-lua/plenary.nvim",
   },
   opts = {
-    interactions = {
-      chat = {
-        adapter = "ollama",
-        model = "gpt-oss:latest",
+    adapters = {
+      http = {
+        ["llama.cpp"] = function()
+          return require("codecompanion.adapters").extend("openai_compatible", {
+            name = "llama.cpp",
+            formatted_name = "llama.cpp",
+            schema = {
+              model = {
+                default = "gpt-oss-20b-mxfp4",
+              },
+            },
+            env = {
+              url = "http://localhost:8081",
+              chat_url = "/v1/chat/completions",
+            },
+          })
+        end,
       },
     },
-    -- NOTE: The log_level is in `opts.opts`
-    opts = {
-      log_level = "DEBUG",
+    interactions = {
+      chat = {
+        adapter = "llama.cpp",
+      },
+      -- NOTE: The log_level is in `opts.opts`
+      opts = {
+        log_level = "DEBUG",
+      },
     },
   },
 }
